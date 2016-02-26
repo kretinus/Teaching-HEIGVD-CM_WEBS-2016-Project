1fedc7404ed1ac7c33e2d19f380714f296aec553
kretinus <kretinus@bluewin.ch>
2016-02-26 14:54:51 +0100

    no message

--- common/models/action.js
+++ common/models/action.js
@@ -1,4 +1,2 @@
 module.exports = function(Action) {
-
- Action.validatesInclusionOf('status', { in: ['comment', 'status-change'] });
 };
--- common/models/issue.js
+++ common/models/issue.js
@@ -1,5 +1,6 @@
 module.exports = function (Issue) {
-  var app = require('../../server/server');  
+     var app = require('../../server/server'); 
+   
 
     Issue.validatesInclusionOf('status', { in: ['created', 'acknowledged', 'assigned', 'in_progress', 'solved', 'rejected'] });
     
@@ -31,9 +32,29 @@ module.exports = function (Issue) {
         );
         
           Issue.observe('before save', function(ctx, next) {
-               console.log(ctx.data);
-               console.log(ctx.currentInstance);
-  next();
+             if (ctx.isNewInstance) {
+                 next();
+             } else {
+   //store the data to action
+   var old_status = ctx.currentInstance.status;
+   var old_date = ctx.currentInstance.date;
+   var old_citizenID = ctx.currentInstance.citizenId;
+   var issueID = ctx.currentInstance.id;
+   var old_data = [old_status, old_date, old_citizenID, issueID];
+   console.log("---          ----");
+   console.log(old_data);   
+  
+   
+   var Action = app.models.Action; 
+   //Issue.app.model.Action.Create({type: ctx.currentInstance.status});
+   Action.create({status: old_status});
+   // Action.create({status: old_status, date: old_date, citizenID: old_citizenID, issueID: issueID});
+      
+   next();
+ 
+             }
 
+              
+  
 });
 };
